379cb42761a5c43dd9f7ef3e48f7137ba2ab0316
Tamir Twina <tamirtw@gmail.com>
2013-11-07 02:08:16 +0200

    Added simple html reponse

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var http = require('http');
 
 pools = {
     'Aloha': 3,
@@ -13,13 +14,41 @@ pools = {
 };
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-for (pool in pools) {
+//Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+
+http.createServer(function (req, res) 
+{
+        var addr = "unknown";
+        var out = "";
+        if (req.headers.hasOwnProperty('x-forwarded-for')) {
+                addr = req.headers['x-forwarded-for'];
+        } else if (req.headers.hasOwnProperty('remote-addr')){
+                addr = req.headers['remote-addr'];
+        }
+
+        if (req.headers.hasOwnProperty('accept')) {
+                if (req.headers['accept'].toLowerCase() == "application/json") {
+                          res.writeHead(200, {'Content-Type': 'application/json'});
+                          res.end(JSON.stringify({'ip': addr}, null, 4) + "\n");                        
+                          return ;
+                }
+        }
+        
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write("Welcome to Node.js on OpenShift!\n\n");
+  for (pool in pools) {
     var url = 'http://www.thprd.org/schedules/schedule.cfm?cs_id=' + pools[pool];
     request(url, function(err, resp, body) {
         if (err)
             throw err;
         $ = cheerio.load(body);
         console.log(pool);
+        res.write(pool + "\n");
         // TODO: scraping goes here!
     });
 }
+  res.end("Your IP address seems to be " + addr + "\n");
+}).listen(port, ipaddr);
+console.log("Server running at http://" + ipaddr + ":" + port + "/");
